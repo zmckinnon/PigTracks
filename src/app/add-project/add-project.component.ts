d32079f278dc1d9b24d3c8643bf7abf0ea24add8
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2021-08-08 20:56:40 -0500

    ✨ Add ability to add a Project (#7)
    * :sparkles: Add ability to add a Project
    
    * :sparkles: Add Name label
    
    * :recycle: Simplify tests

---
+++ src/app/add-project/add-project.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Project } from '../project';
+import { ProjectService } from '../project.service';
+
+@Component({
+  selector: 'app-add-project',
+  templateUrl: './add-project.component.html',
+  styleUrls: ['./add-project.component.scss']
+})
+export class AddProjectComponent {
+  model: Project = new Project();
+
+  constructor(private router: Router, private projectService: ProjectService) { }
+
+  onSave(): void {
+    this.projectService.addProject(this.model)
+      .subscribe(project => {
+        this.router.navigate(['/project', project.id]);
+      });
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProjectComponent } from './add-project/add-project.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 
 const routes: Routes = [
   {path: 'projects', component: ProjectListComponent},
+  {path: 'project/add', component: AddProjectComponent},
   {path: 'project/:id', component: ProjectDetailsComponent}
 ];
 
